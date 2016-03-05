bb5e67403daee0a100e78736f941bae8614fb441
Dan Shahin <dan@codescience.com>
2016-03-05 13:46:54 -0800

    refactor auth

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 var ref = new Firebase('https://glaring-inferno-5854.firebaseio.com/');
 
+
+ref.onAuth(onAuthCallback);
+ref.offAuth(onAuthCallback);
+
+ref.on('child_added', function(snapshot) {
+	var message = snapshot.val();
+	displayChatMessage(message.name, message.text);
+});
+
+
 $('#messageInput').keypress(function (e) {
     if (e.keyCode == 13) {
       var name = $('#nameInput').val();
@@ -9,14 +19,22 @@ $('#messageInput').keypress(function (e) {
     }
 });
 
-$('button.github').click(function(){
-	ref.authWithOAuthRedirect("github", 
+$('button.logout').click(function(){
+	ref.unauth();
+});
+
+$('button.login').click(function(){
+	var $btn = $(this),
+		provider = $btn.data('provider');
+
+	ref.authWithOAuthRedirect(provider, 
 		function(error, authData) {
 			if (error) {
 			    toastr.error("Authentication Failed!", error);
 			} else {
-			    toastr.success("Authenticated to github" );
-			    console.log('authData', authData);
+				// We'll never get here, as the page will redirect on success.
+			    // toastr.success("Authenticated to github" );
+			    // console.log('authData', authData);
 			}
 		},
 		//additional oauth parameters
@@ -27,10 +45,36 @@ $('button.github').click(function(){
 	);
 });
 
-ref.on('child_added', function(snapshot) {
-	var message = snapshot.val();
-	displayChatMessage(message.name, message.text);
-});
+function onAuthCallback(authData){
+	if (authData ) {
+	    // save the user's profile into the database so we can list users,
+	    // use them in Security and Firebase Rules, and show profiles
+	    console.log("Authenticated user ", authData);
+	    
+	    ref.child("users").child(authData.uid).set({
+	      provider: authData.provider,
+	      name: getName(authData),
+	      time : new Date().toUTCString()
+	    });
+
+	}else{
+		console.log('Unauthenticated user');
+	}
+}
+
+// find a suitable name based on the meta info given by each provider
+function getName(authData) {
+  switch(authData.provider) {
+     case 'password':
+       return authData.password.email.replace(/@.*/, '');
+     case 'twitter':
+       return authData.twitter.displayName;
+     case 'facebook':
+       return authData.facebook.displayName;
+     case 'github':
+       return authData.github.displayName;
+  }
+}
 
 function displayChatMessage(name, text) {
     $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
